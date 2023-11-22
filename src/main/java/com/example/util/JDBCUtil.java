afcc75c11b1d66a3486ee7e63cb39c92cbc2636c
jinsejin <j03298969@gmail.com>
2023-11-22 19:34:05 +0900

    [feat] project 4_2 crud 기능 추가

--- src/main/java/com/example/util/JDBCUtil.java
+++ src/main/java/com/example/util/JDBCUtil.java
@@ -4,21 +4,22 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class JDBCUtil {
-    public static Connection getConnection(){
-        Connection con = null;
-        try{
-            Class.forName("com.mysql.jdbc.Driver");
-            con = DriverManager.getConnection("jdbc:mysql://walab.handong.edu:3306/p231_21800712", "p231_21800712", "jiex3W");
-        }catch(Exception e){
-            System.out.println(e);
-        }
-        return con;
-    }
-     public static void main(String args[]){
-         Connection conn = getConnection();
-         if(conn != null)
-             System.out.println("DB 연결됨!");
-         else
-             System.out.println("DB 연결중 오류 발생!");
-     }
+	public static Connection getConnection(){  
+	    Connection con=null;  
+	    try{  
+	        Class.forName("com.mysql.jdbc.Driver");  
+	        con= DriverManager.getConnection("jdbc:mysql://db4free.net:3306/dbname","dbid","idpwd");  
+	    }catch(Exception e){
+	    	System.out.println(e);
+	    }  
+	    return con;  
+	}  
+	
+//	public static void main(String ars[]) {
+//		Connection conn = getConnection();
+//		if(conn != null)
+//			System.out.println("DB 연결됨!");
+//		else
+//			System.out.println("DB 연결중 오류 !");
+//	}
 }
